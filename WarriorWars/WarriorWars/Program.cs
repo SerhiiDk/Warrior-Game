24b1b991cd8d3a6293f1a99d7ebbbbc7f979d9ff
Serhii <94891317+SerhiiDk@users.noreply.github.com>
2022-05-11 21:32:35 +0200

    Added Game

---
+++ WarriorWars/WarriorWars/Program.cs
@@ -0,0 +1,29 @@
+﻿using WarriorWars;
+using WarriorWars.Enum;
+
+namespace WarrirWars
+{
+    class EntryPoint
+    {
+        static Random rng = new Random();
+
+        static void Main()
+        {
+            Warrior goodGuy = new Warrior("Bob", Faction.GoogGuy);
+            Warrior badGuy = new Warrior("Joe", Faction.BadGuy);
+
+            
+            while(goodGuy.IsAlive && badGuy.IsAlive)
+            {
+                if (rng.Next(0,10) < 5)
+                {
+                    goodGuy.Attack(badGuy);
+                }
+                else
+                {
+                    badGuy.Attack(goodGuy);
+                }
+            }
+        } 
+    }
+}
